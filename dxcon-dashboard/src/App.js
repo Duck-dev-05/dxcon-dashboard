50ca96256612fac563335432c10ff765bf5ba631
Duckdev <minhducworking05@gmail.com>
2024-11-20 16:13:34 +0700

    Upbase

--- dxcon-dashboard/src/App.js
+++ dxcon-dashboard/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="App">
       <Sidebar />
-      <div className="main-content">
+      <div className="main">
         <Header />
       </div>
     </div>
